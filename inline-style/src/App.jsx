07a9987eb9986acf6cf2651655e8fd7dd38b331e
Honma Ryota <79443099+HonmaRyota@users.noreply.github.com>
2022-02-12 01:21:55 +0900

    styled-css test

--- inline-style/src/App.jsx
+++ inline-style/src/App.jsx
@@ -1,12 +1,14 @@
 import {InlineStyle} from "./components/InlineStyle";
 import "./App.css";
 import {CssModules} from "./components/CssModules";
+import {StyledJsx} from "./components/StyledJdx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
